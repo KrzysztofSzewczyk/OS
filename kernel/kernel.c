3ca6d9d70801df2af526e784b5dfd83ef24e8011
Krzysztof Szewczyk <marekszuwarek1958@gmail.com>
2017-12-10 16:03:20 +0100

    Kernel.c cleanup

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -12,44 +12,39 @@
 
 /*Funkcje z LIBC. Kopiowklejka*/
 
+unsigned short *textmemptr;
+int attrib = 0x0F;
+int csr_x = 0, csr_y = 0;
+
 void *memcpy(void *dest, const void *src, size_t count) {
     const char *sp = (const char *)src;
     char *dp = (char *)dest;
     for(; count != 0; count--) *dp++ = *sp++;
     return dest;
 }
-
 void *memset(void *dest, char val, size_t count) {
     char *temp = (char *)dest;
     for( ; count != 0; count--) *temp++ = val;
     return dest;
 }
-
 unsigned short *memsetw(unsigned short *dest, unsigned short val, size_t count) {
     unsigned short *temp = (unsigned short *)dest;
     for( ; count != 0; count--) *temp++ = val;
     return dest;
 }
-
 size_t strlen(const char *str) {
     size_t retval;
     for(retval = 0; *str != '\0'; str++) retval++;
     return retval;
 }
-
 unsigned char inb (unsigned short _port) {
     unsigned char rv;
     __asm__ __volatile__ ("inb %1, %0" : "=a" (rv) : "dN" (_port));
     return rv;
 }
-
 void outb (unsigned short _port, unsigned char _data) {
     __asm__ __volatile__ ("outb %1, %0" : : "dN" (_port), "a" (_data));
 }
-
-unsigned short *textmemptr;
-int attrib = 0x0F;
-int csr_x = 0, csr_y = 0;
 void scroll(void) {
     unsigned blank, temp;
     blank = 0x20 | (attrib << 8);
@@ -60,7 +55,6 @@ void scroll(void) {
         csr_y = 25 - 1;
     }
 }
-
 void move_csr(void) {
     unsigned temp;
     temp = csr_y * 80 + csr_x;
@@ -69,7 +63,6 @@ void move_csr(void) {
     outb(0x3D4, 15);
     outb(0x3D5, temp);
 }
-
 void cls() {
     unsigned blank;
     int i;
@@ -80,7 +73,6 @@ void cls() {
     csr_y = 0;
     move_csr();
 }
-
 void putchar(unsigned char c) {
     unsigned short *where;
     unsigned att = attrib << 8;
@@ -122,7 +114,6 @@ void initdisplay(void) {
     textmemptr = (unsigned short *)0xB8000;
     cls();
 }
-
 void kmain(void) {
 	initdisplay();
 	puts("Hello, kernel World!\n");
