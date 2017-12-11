f4a2fee1f2a8424760c5ba308bacf74cc9899253
Krzysztof Szewczyk <marekszuwarek1958@gmail.com>
2017-12-11 15:29:29 +0100

    Structuring code

--- kernel/OS.h
+++ kernel/OS.h
@@ -8,12 +8,36 @@ struct regs {
     unsigned int int_no, err_code;
     unsigned int eip, cs, eflags, useresp, ss;    
 };
+struct idt_entry {
+    unsigned short base_lo;
+    unsigned short sel;
+    unsigned char always0;
+    unsigned char flags;
+    unsigned short base_hi;
+} __attribute__((packed));
+struct idt_ptr {
+    unsigned short limit;
+    unsigned int base;
+} __attribute__((packed));
 
 void *memcpy(void *dest, const void *src, size_t count);
 void *memset(void *dest, char val, size_t count);
 unsigned short *memsetw(unsigned short *dest, unsigned short val, size_t count);
 size_t strlen(const char *str);
-unsigned char inportb (unsigned short _port);
-void outportb (unsigned short _port, unsigned char _data);
-
+unsigned char inb (unsigned short _port);
+void outb (unsigned short _port, unsigned char _data);
+int atoi(register char *string);
+void handle_timer(struct regs *r);
+void timer_wait(int ticks);
+void timer_install(void);
+char scancodeToCharacter (char scancode);
+char getScancode(void);
+void scroll(void);
+void move_csr(void);
+void cls();
+void putchar(unsigned char c);
+void puts(unsigned char *text);
+void color(unsigned char forecolor, unsigned char backcolor);
+void initdisplay(void);
+int time(void);
 #endif
--- kernel/szewczykmake.sh
+++ kernel/szewczykmake.sh
@@ -1,3 +1,6 @@
 ./../../WinGCC/bin/i686-elf-as boot.s -o boot.o
-./../../WinGCC/bin/i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O0 -Wall -Wextra
-./../../WinGCC/bin/i686-elf-gcc -T linker.ld -o kernel.com -ffreestanding -O0 -nostdlib boot.o kernel.o -lgcc
+./../../WinGCC/bin/i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O0
+./../../WinGCC/bin/i686-elf-gcc -c libc.c -o libc.o -std=gnu99 -ffreestanding -O0
+./../../WinGCC/bin/i686-elf-gcc -c keyboard.c -o keyboard.o -std=gnu99 -ffreestanding -O0
+./../../WinGCC/bin/i686-elf-gcc -c video.c -o video.o -std=gnu99 -ffreestanding -O0
+./../../WinGCC/bin/i686-elf-gcc -T linker.ld -o kernel.com -ffreestanding -O0 -nostdlib boot.o libc.o video.o keyboard.o kernel.o -lgcc
