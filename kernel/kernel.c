a116714f94a6f2045ed63d81c20ac45feae5b23d
Palaiologos <marekszuwarek1958@gmail.com>
2018-09-29 12:04:45 +0200

    Do we really need that loop?

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -6,10 +6,9 @@ void reboot(void) {
     while (good & 0x02)
         good = inb(0x64);
     outb(0x64, 0xFE);
-    for(;;);
 }
 
 void kmain(void) {
 	initdisplay();
-	puts("They see me runnin', they hatin'!\n");
+	puts("Working!\n");
 }
